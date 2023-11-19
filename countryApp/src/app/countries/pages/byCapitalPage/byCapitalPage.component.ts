759d1e8f79f64ae3fbaa3e02481ad64199688bc0
Santiago Cante <114036508+CanteSantiago@users.noreply.github.com>
2023-11-19 17:07:23 -0500

    Vid121-FuncionamientoDelSearchBox

--- countryApp/src/app/countries/pages/byCapitalPage/byCapitalPage.component.ts
+++ countryApp/src/app/countries/pages/byCapitalPage/byCapitalPage.component.ts
@@ -5,8 +5,10 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './byCapitalPage.component.html',
 })
 export class ByCapitalPageComponent {
+  searchByCapital( term: string ):void  {
+console.log('Desde ByCapitalPage');
+console.log({term});
 
-  constructor() { }
-
+}
 
 }
--- countryApp/src/app/shared/components/searchBox/searchBox.component.ts
+++ countryApp/src/app/shared/components/searchBox/searchBox.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'shared-search-box',
@@ -8,5 +8,12 @@ export class SearchBoxComponent  {
 
 @Input()
 public placeholder:string='';
+
+@Output()
+  public onValue = new EventEmitter<string>();
+
+  emitValue( value: string ):void {
+    this.onValue.emit( value );
+  }
 
 }
