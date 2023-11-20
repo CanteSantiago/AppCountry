d86448243441649a82db028fcb431e8c57cc2720
Santiago Cante <114036508+CanteSantiago@users.noreply.github.com>
2023-11-19 22:48:34 -0500

    Vid138-Debounce
    Peticiones para cuando el usuario debe de escribir

--- countryApp/src/app/shared/components/searchBox/searchBox.component.ts
+++ countryApp/src/app/shared/components/searchBox/searchBox.component.ts
@@ -1,10 +1,16 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Subject, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './searchBox.component.html',
 })
-export class SearchBoxComponent  {
+export class SearchBoxComponent  implements OnInit{
+
+
+
+  private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSuscription?: Subscription;
 
 @Input()
 public placeholder:string='';
@@ -12,8 +18,27 @@ public placeholder:string='';
 @Output()
   public onValue = new EventEmitter<string>();
 
+  @Output()
+  public onDebounce = new EventEmitter<string>();
+
+
+ngOnInit(): void {
+  this.debouncer
+  .pipe(
+    debounceTime(300)
+  )
+  .subscribe(value =>{
+   this.onDebounce.emit(value);
+  })
+}
+
+
   emitValue( value: string ):void {
     this.onValue.emit( value );
+  }
+
+  onKeyPress( searchTerm: string ) {
+    this.debouncer.next( searchTerm );
   }
 
 }
