3f09aff046b8dbc9a1a1df7eaffafec8c1100b9e
Santiago Cante <114036508+CanteSantiago@users.noreply.github.com>
2023-11-20 01:28:55 -0500

    Vid140-BuscarPorRegion

--- countryApp/src/app/countries/pages/byRegionPage/byRegionPage.component.ts
+++ countryApp/src/app/countries/pages/byRegionPage/byRegionPage.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountricesService } from '../../services/countries.service';
 
+type Region = 'Africa'| 'Americas'| 'Asia'| 'Europe'| 'Oceania';
+
 @Component({
   selector: 'app-byRegionPage',
   templateUrl: './byRegionPage.component.html',
@@ -9,10 +11,17 @@ import { CountricesService } from '../../services/countries.service';
 export class ByRegionPageComponent {
 
   public countries: Country[] = [];
+  public regions: Region[]= ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
+
 
   constructor( private CountriesService: CountricesService ) {}
 
-  searchByRegion( region: string ):void  {
+  searchByRegion( region: Region ):void  {
+
+    this.selectedRegion = region;
+
+
     this.CountriesService.searchRegion( region )
       .subscribe( countries => {
         this.countries = countries;
