229ca1acbf1846a25d7a2dbd55f0872b71665e73
Santiago Cante <114036508+CanteSantiago@users.noreply.github.com>
2023-11-19 21:49:01 -0500

    Vid135-RefactorizacionDeServicio
    crear getCountriesRequest para tener una mejor lectura de codigo

--- countryApp/src/app/countries/services/countries.service.ts
+++ countryApp/src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, map, of } from 'rxjs';
+import { Observable, catchError, delay, map, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -8,18 +8,14 @@ export class  CountricesService {
   private apiUrl: string = 'https://restcountries.com/v3.1'
   constructor(private http: HttpClient) { }
 
+private getCountriesRequest(url: string):Observable<Country[]>{
+  return this.http.get<Country[]>(url)
+  .pipe(
+    catchError( () => of([]) ),
+    delay(2000),
+  );
+}
 
-  // searchCountryByAlphaCode( code: string ): Observable<Country | null> {
-
-  //   const url = `${ this.apiUrl }/alpha/${ code }`;
-
-  //   return this.http.get<Country[]>( url )
-  //     .pipe(
-  //       map( countries = countries.length > 0 ? countries[0]: null ),
-
-  //       catchError( () => of(null) )
-  //     );
-  // }
 searchCountrByAlphaCode(code:string):Observable<Country | null>{
   const url = `${ this.apiUrl }/alpha/${ code }`;
   return  this.http.get<Country[]>(url)
@@ -32,32 +28,24 @@ searchCountrByAlphaCode(code:string):Observable<Country | null>{
   searchCapital( term: string ):Observable<Country[]> {
 
     const url = `${ this.apiUrl }/capital/${ term }`;
-    return this.http.get<Country[]>( url )
-    .pipe(
+   return this.getCountriesRequest(url);
     // tap( countries =>console.log('Paso por el tap1', countries)),
     // map(countries => []),
     // tap( countries =>console.log('Paso por el tap2', countries)),
-    catchError( () => of([]) )
-    );
+
 }
 
 
 searchCountry( term: string ): Observable<Country[]> {
 
   const url = `${ this.apiUrl }/name/${ term }`;
-  return this.http.get<Country[]>( url )
-    .pipe(
-      catchError( () => of([]) )
-    );
+  return this.getCountriesRequest(url);
 }
 
 searchRegion( region: string ): Observable<Country[]> {
 
   const url = `${ this.apiUrl }/region/${ region }`;
-  return this.http.get<Country[]>( url )
-    .pipe(
-      catchError( () => of([]) )
-    );
+  return this.getCountriesRequest(url);
 }
 
 
